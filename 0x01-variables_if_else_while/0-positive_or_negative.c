756bac19d8adaa835b56a04e7ac821d6d78727c7
imohski <imohidang@gmail.com>
2022-10-23 18:58:43 -0700

    Programs

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,11 +17,11 @@ int main(void)
 	if (n < 0)
 	{
 		printf("%d is negative\n", n);
-	} 
+	}
 	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
-	} 
+	}
 	else
 	{
 		printf("%d is positive\n", n);
